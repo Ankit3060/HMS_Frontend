0a8df5b5975eb41eb3da75e4c92c6abe11ec691b
Ankit3060 <ankit330660@gmail.com>
2024-10-22 22:17:40 +0530

    Frontend done and ready to deploy

---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Biography from '../components/Biography'
+import Departments from '../components/Departments'
+import MessageForm from '../components/MessageForm'
+import { useContext } from 'react'
+import { Context } from '../main'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+function Home() {
+  const {isAuthenticated, setIsAuthenticated} = useContext(Context);
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "https://hms-backend-deployment-vcal.onrender.com/api/v1/user/patient/me",
+          // `${window.location.origin}/api/v1/user/patient/me`,
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  },[isAuthenticated,setUser])
+  
+
+
+  return (
+    <>
+    <Hero title={`Welcome to CU Hospital | ${user.firstName} ${user.lastName} | Your trusted healthcare provider`} imageUrl={"/hero.png"} />
+    <Biography imageUrl={"/about.png"}/>
+    <Departments />
+    <MessageForm />
+    </>
+  )
+}
+
+export default Home
